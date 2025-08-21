bdc141e853145f75f8875a8d1a6124a2b622ebc5
Brandon-git-hub <brandon410423@gmail.com>
2025-08-21 18:04:40 +0800

    週四 2025/08/21

---
+++ Keil_VS_Open.bat
@@ -0,0 +1,8 @@
+@echo off
+REM 啟動 Visual Studio 2022
+start "" "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
+
+REM 啟動 Keil uVision4
+start "" "C:\Users\brandon_wu\AppData\Local\Keil_v5\UV4\UV4.exe"
+
+exit
