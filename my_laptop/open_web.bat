3cfe04ead8dd21136cd9ea624eeb8522f6b615b0
Brandon-git-hub <brandon410423@gmail.com>
2025-09-08 00:11:35 +0800

    08/09/2025

--- my_laptop/open_web.bat
+++ my_laptop/open_web.bat
@@ -4,6 +4,7 @@ REM 使用預設瀏覽器打開指定網址
 start "" "https://space.bilibili.com/487515243"
 start "" "https://hdlbits.01xz.net/wiki/Main_Page"
 start "" "https://github.com/Brandon-git-hub/Blog"
+start "" "https://leetcode.com/problemset/"
 
 REM 等待 3 秒再結束視窗（可依需要調整或刪除）
 timeout /t 3 >nul
