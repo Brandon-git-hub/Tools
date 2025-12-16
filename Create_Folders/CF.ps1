5bbfef0db22cb9b50141c118c01235673e5a8f1f
Brandon-git-hub <brandon410423@gmail.com>
2025-12-16 10:42:44 +0800

    週二 2025/12/16

--- Create_Folders/CF.ps1
+++ Create_Folders/CF.ps1
@@ -12,7 +12,8 @@ if (-not $Paths -or $Paths.Count -eq 0) {
 $FolderNumbers = "18", "19", "1A", "1B", "1C", "1D"
 
 # 定義每個資料夾內的子資料夾名稱
-$SubFolder = "Scan_VPP"
+# $SubFolder = "Scan_VPP"
+$SubFolder = "dump"
 
 foreach ($p in $Paths) {
     $TargetDir = $p
@@ -25,12 +26,13 @@ foreach ($p in $Paths) {
 
         # 2. 組合完整的路徑，直接包含子資料夾，並用 New-Item -Force 建立
         # 範例路徑: C:\Users\brandon_wu\...\#30\dump
-        $FullPathWithSubFolder1 = Join-Path -Path $TargetDir -ChildPath "$FolderName\$SubFolder\3_12V"
-        $FullPathWithSubFolder2 = Join-Path -Path $TargetDir -ChildPath "$FolderName\$SubFolder\11_3V"
-        # $FullPathWithSubFolder = Join-Path -Path $TargetDir -ChildPath "$FolderName\"
+        # $FullPathWithSubFolder1 = Join-Path -Path $TargetDir -ChildPath "$FolderName\$SubFolder\3_12V"
+        # $FullPathWithSubFolder2 = Join-Path -Path $TargetDir -ChildPath "$FolderName\$SubFolder\11_3V"
+        $FullPathWithSubFolder = Join-Path -Path $TargetDir -ChildPath "$FolderName\$SubFolder\"
 
         # New-Item 加上 -Force 會自動建立所有不存在的父目錄和子目錄
-        New-Item -ItemType Directory -Path $FullPathWithSubFolder1 -Force | Out-Null
-        New-Item -ItemType Directory -Path $FullPathWithSubFolder2 -Force | Out-Null
+        New-Item -ItemType Directory -Path $FullPathWithSubFolder -Force | Out-Null
+        # New-Item -ItemType Directory -Path $FullPathWithSubFolder1 -Force | Out-Null
+        # New-Item -ItemType Directory -Path $FullPathWithSubFolder2 -Force | Out-Null
     }
 }
